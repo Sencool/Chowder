1c5403acde327bc012b8485f32d73e1c19dac3af
tangdx <1214632164@qq.com>
2024-02-03 20:50:30 +0800

    feat: 倒计时组件开发

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Link from 'COMMON/components/MyLink'
+import CountDownButton from 'COMMON/components/CountDownButton'
 import nav from './navMeta.js'
 import Routers from './routers.jsx'
 import styles from './app.scss'
@@ -11,47 +12,23 @@ export const UserContext = React.createContext('')
 function App() {
     return (
         <div>
-        <UserContext.Provider value={'chuanshi'}>
-            {/* <GetCodeButton /> */}
-            <div className={styles.nav}>
-                {nav.map((item) => {
-                    const { to, name } = item
-                    return (
-                        <Link key={to} to={to} className={styles.navTab}>
-                            {name}
-                        </Link>
-                    )
-                })}
-            </div>
-            <Routers />
-        </UserContext.Provider>
+            <UserContext.Provider value={'chuanshi'}>
+                <CountDownButton />
+                <div className={styles.nav}>
+                    {nav.map((item) => {
+                        const { to, name } = item
+                        return (
+                            <Link key={to} to={to} className={styles.navTab}>
+                                {name}
+                            </Link>
+                        )
+                    })}
+                </div>
+                <Routers />
+            </UserContext.Provider>
         </div>
     )
 }
 
 export default App
 
-function GetCodeButton() {
-    const [countDown, setCountDown] = useState(0)
-    const handleClick = () => {
-        setCountDown(60)
-    }
-
-    useEffect(() => {
-        if (countDown > 0) {
-            setTimeout(() => {
-                setCountDown(countDown - 1)
-            }, 1000)
-        }
-    }, [countDown])
-
-    return (
-        <>
-            {countDown === 0 ? (
-                <div onClick={handleClick}>获取验证码</div>
-            ) : (
-                <div>{countDown}秒后可以再次发送</div>
-            )}
-        </>
-    )
-}
